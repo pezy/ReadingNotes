45fafa9e642b15aa8839cddb2b230c7d105eae8a
pezy_mbp <urbancpz@gmail.com>
2014-07-09 22:14:52 +0800

    add more template functions.

--- Template/Template/main.cpp
+++ Template/Template/main.cpp
@@ -7,8 +7,19 @@
 //
 
 #include <iostream>
+#include <cstring>
 #include <string>
 #include "max.h"
+
+template <typename T>
+inline T* const& max(T* const& a, T* const& b) {
+    return *a < *b ? b : a;
+}
+
+inline char const* const& max(char const* const& a, char const* const& b)
+{
+    return std::strcmp(a, b) < 0 ? b : a;
+}
 
 int main(int argc, const char * argv[])
 {
@@ -22,6 +33,23 @@ int main(int argc, const char * argv[])
     std::string s1 = "mathematics";
     std::string s2 = "math";
     std::cout << "max(s1,s2) : " << ::max(s1, s2) << std::endl;
+    
+    int a = 7;
+    int b = 42;
+    
+    std::cout << ::max(a,b) << std::endl;
+    
+    std::string s = "hey";
+    std::string t = "you";
+    std::cout << ::max(s, t) << std::endl;
+    
+    int* p1 = &b;
+    int* p2 = &a;
+    std::cout << *(::max(p1, p2)) << std::endl;
+    
+    char const* sz1 = "David";
+    char const* sz2 = "Nico";
+    std::cout << ::max(sz1, sz2) << std::endl;
     
     return 0;
 }

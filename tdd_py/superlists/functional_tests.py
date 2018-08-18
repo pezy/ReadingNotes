f101ef9455d05565245d1da5f2f2a31ab3c7291e
pezy_mbp <urbancpz@gmail.com>
2018-08-18 23:40:08 +0800

    use unittest to write a standard test.

--- tdd_py/superlists/functional_tests.py
+++ tdd_py/superlists/functional_tests.py
@@ -1,6 +1,14 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn('To-Do', self.browser.title)
 
-assert 'Django' in browser.title
+if __name__ == '__main__':
+    unittest.main()
